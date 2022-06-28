d4a90b5da97e5b1596271484081dc16d79a0493d
ASHOK KUMAR <96563901+ASHOK44321@users.noreply.github.com>
2022-06-28 14:36:48 +0530

    N=int(input()) for i in range(N):     for j in range(i+1):         print(j+1,end="")     print("")

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -1,5 +1,5 @@
 N=int(input())
-for i in range(1,N+1):
-    for j in range(1,N+1):
-        print(chr(64+i),end=" ")
-    print()
+for i in range(N):
+    for j in range(i+1):
+        print(j+1,end="")
+    print("")
