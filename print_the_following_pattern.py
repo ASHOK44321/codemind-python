4fd3427caedadd3eb866f53a4d36bb22746f1171
ASHOK KUMAR <96563901+ASHOK44321@users.noreply.github.com>
2022-06-28 14:24:08 +0530

    N=int(input()) for i in range(1,N+1):     for j in range(1,N-1):         print(j,end="")     for k in range(N-3,0,-1):         print(k,end="")     print()

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -1,8 +1,7 @@
 N=int(input())
 for i in range(1,N+1):
-    for j in range(1,N+1):
-        if i==N or j==1 or i==j:
-            print("*",end="")
-        else:
-            print(end=" ")
+    for j in range(1,N-1):
+        print(j,end="")
+    for k in range(N-3,0,-1):
+        print(k,end="")
     print()
